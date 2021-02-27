4013e45c1eaea35724a49c6c8b11d7cf24f63373
HendraPutra563 <78998317+Ependelope53@users.noreply.github.com>
2021-02-27 12:13:48 +0700

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -26,16 +26,28 @@ from userbot.events import register
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 
 DEF_UNAPPROVED_MSG = (
-    "**▬▬▬▬▬▬❙۩♛۩❙▬▬▬▬▬▬**\n"
-    f"**Welcome To Room Chat** **{DEFAULTUSER} 🤗**\n"
-    f"**Saya Adalah Bot Yang Menjaga Room Chat** **Bos {DEFAULTUSER}💀.** "
-    "**Mohon Jangan Melakukan Spam Di Room Chat Bos😈** "
-    "**Karna Jika Anda Melakukan Itu Saya Akan Otomatis** "
-    f"**Memblokir Anda🙌**, **Tunggulah Sampai** **Bos {DEFAULTUSER}** "
-    "**Menerima** **Pesan Anda🙂**, **Terimakasih ヅ**\n\n"
-    "**PESAN OTOMATIS**"
-    "\n━━━━━━━━━━━━━━━"
-    "\n**▬▬▬▬▬▬❙۩★۩❙▬▬▬▬▬▬**")
+     ╭━━━━━━━━━━━━━━━━━━━━━╮\n"
+    "    SELAMAT DATANG DI\n"
+    f"   `{DEFAULTUSER}` CHAT\n"
+    "╰━━━━━━━━━━━━━━━━━━━━━╯\n\n"
+
+    f"Saya Bot Assisten `{DEFAULTUSER}`\n"
+    "╾─────────────────────╼\n\n"
+
+    "╭━━━━━━━━━━━━━━━━━━━━━╮\n"
+    " Mohon untuk tidak\n"
+    " melakukan spam chat\n"
+    " Atau Saya Akan Otomatis\n"
+    " Mereport & Block Kamu\n"
+    " Silahkan Tungguu Sampai\n"
+    f" `{DEFAULTUSER}` Menerima Pesanmu\n"
+    "╰━━━━━━━━━━━━━━━━━━━━━╯\n\n"
+
+    "╾─────────────────────╼\n"
+    "   🌻🌻 TERIMAKASIH 🌻🌻\n"
+    "─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇─̇\n"
+    f"- ©Bot Assisten `{DEFAULTUSER}`\n"
+    "- Protected By 🔥GabutC-UBot🔥")
 # =================================================================
 
 
