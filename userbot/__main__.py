129201d34c195194a590f00e9974c0dbda438b7b
vellovernn <89903064+vellovernn@users.noreply.github.com>
2021-09-26 01:48:27 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"GabutC-UBot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"MUGEN ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
